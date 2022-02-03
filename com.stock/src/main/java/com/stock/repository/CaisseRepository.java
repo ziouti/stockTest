5e69d17600b4de3dbc62e9a9bea5b29d934fd093
hamza ziouti <hamza.ziouti@indatacore.com>
2022-02-03 04:36:39 +0100

    jwt authentication added and Repository and CaisseService updated

--- com.stock/src/main/java/com/stock/repository/CaisseRepository.java
+++ com.stock/src/main/java/com/stock/repository/CaisseRepository.java
@@ -2,11 +2,15 @@ package com.stock.repository;
 
 import com.stock.model.Caisse;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-@Repository
+
 public interface CaisseRepository extends JpaRepository<Caisse, String> {
 
     List<Caisse> findByNumberBouteilleShortagedNot(int numberBouteilleShortaged);
+
+
+    @Query(value = "select * from cb_stock_register where id_caisse not in (select distinct(id_caisse) from cb_bottle_register)", nativeQuery = true)
+    List<Caisse> findAllCaisseWithouBotelles();
 }
--- com.stock/src/main/java/com/stock/service/CaisseService.java
+++ com.stock/src/main/java/com/stock/service/CaisseService.java
@@ -30,18 +30,13 @@ public class CaisseService {
     private StatusRepository statusRepository;
 
     public Caisse saveCaisse(List<CreateDto> dtoToSave) {
-  
-		Integer nbrBouiteilleDamaged = dtoToSave.stream()
-                .filter(dto -> dto.getEtat().equals(statusRepository.findByLibelle(STATUS_DAMAGED))
-                		)
-                .collect(Collectors.toList())
-                .size();
+        int nmbrBottlsDamaged = countNombreDamagedByCaisse(dtoToSave);
 
         Caisse caisse = Caisse.builder()
                 .idCaisse(UUID.randomUUID().toString())
-                .numberBouteilleDamaged(nbrBouiteilleDamaged)
-                .numberBouteilleShortaged(12 - dtoToSave.size())
-                .status(getStatusCaisse(dtoToSave.size()))
+                .numberBouteilleDamaged(nmbrBottlsDamaged)
+                .numberBouteilleShortaged(NBR_BOTTLE_CAISSE - dtoToSave.size())
+                .status(getStatusCaisse(NBR_BOTTLE_CAISSE - dtoToSave.size()))
                 .build();
 
         List<Bouteille> bouiteillesToSave = dtoToSave.stream()
@@ -66,7 +61,7 @@ public class CaisseService {
         for (int i=0; i <= allBouteilleFromInComplete.size()/NBR_BOTTLE_CAISSE  ;i++){
             Caisse caisseToUpdate = caisseInComplete.get(i);
 
-            for (int j=i*NBR_BOTTLE_CAISSE; j < (i+1)*NBR_BOTTLE_CAISSE && j < allBouteilleFromInComplete.size() ;j++){
+            for (int j=i*NBR_BOTTLE_CAISSE; j < (i+1)*NBR_BOTTLE_CAISSE && j<allBouteilleFromInComplete.size() ;j++){
                 Bouteille bouteilleToUpdate = allBouteilleFromInComplete.get(j);
 
                 if(!caisseToUpdate.getListBottles().contains(bouteilleToUpdate)){
@@ -93,17 +88,18 @@ public class CaisseService {
     }
 
     private String getStatusCaisse(int size) {
-        String statusCaisse = size == NBR_BOTTLE_CAISSE ? STATUS_COMPLETED : STATUS_SHORTEGED;
-        return statusCaisse;
+        return size == 0 ? STATUS_COMPLETED : STATUS_SHORTEGED;
+    }
+
+    private int countNombreDamagedByCaisse(List<CreateDto> dtoToSave) {
+         return dtoToSave.stream()
+                .filter(dto -> dto.getEtat().equals(statusRepository.findByLibelle(STATUS_DAMAGED).getLibelle()))
+                .collect(Collectors.toList())
+                .size();
     }
 
     private void deleteEmptyCaisse() {
-        List<Caisse> emptyCaisses = caisseRepository.findAll().stream()
-                .filter(caisse -> caisse.getListBottles() == null
-                        || caisse.getListBottles().isEmpty()
-                        || caisse.getListBottles().size() == 0)
-                .collect(Collectors.toList());
-
-        emptyCaisses.forEach(caisse -> caisseRepository.delete(caisse));
+        List<Caisse> emptyCaisses = caisseRepository.findAllCaisseWithouBotelles();
+        emptyCaisses.forEach(caisse -> caisseRepository.deleteById(caisse.getIdCaisse()));
     }
 }
